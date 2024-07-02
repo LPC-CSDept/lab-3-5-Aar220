3a89f1bdc8e7fbeaf2cd75cf5b1b32dba0db02c6
Aar220 <aaryanboreddy722@gmail.com>
2024-07-02 00:01:07 -0700

    CH3L5_Done

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ def main():
     num1 = int(input('Enter the first number: '))
     num2 = int(input('Enter the second number: '))
     num3 = int(input('Enter the third number: '))
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-    duplication = 0
+
+    if num1 == num2 and num2 == num3:
+        duplication = 3
+    elif num1 != num2 and num2 != num3:
+        duplication = 0
+    else:
+        duplication = 2
+   
+    print duplication
 
     ########################################
     # Do not delete the return statement
@@ -17,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
